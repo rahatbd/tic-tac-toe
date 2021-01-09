7571f88890d2bc09baec4cef8b83e1185f567f6a
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2021-01-08 19:13:34 -0500

    added keyboard navigation

--- script.js
+++ script.js
@@ -5,27 +5,50 @@ tictactoeApp.count = 0;
 $gridSpan = $('.grid span');
 
 tictactoeApp.play = () => {
+    let spanID = 0;
     $h2Span = $('h2 span');
+
+    $gridSpan.keypress(function (event) {
+        //check
+        if (!$('input[type=checkbox]').prop('checked')) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                spanID = $(this).attr('id');
+                if (!tictactoeApp.match()) {
+                    game();
+                }
+            }
+        }
+    })
+
     $gridSpan.on('click', function () {
-        //checkbox
-        $('input[type=checkbox]').attr('disabled', true);
+        spanID = $(this).attr('id');
+        // game();
+        if (!tictactoeApp.match()) {
+            game();
+        }
+        else {
+            $gridSpan.off('click');
+        }
+    })
+
+    const game = () => {
         const h2ID = $h2Span.attr('id');
-        const spanID = $(this).attr('id');
+        //disabling random checkbox if not selected before tap or click
+        $('input[type=checkbox]').attr('disabled', true);
         if (spanID in tictactoeApp.spanIDObj) {
             alert('Click another tile.');
         }
         else {
             if (h2ID === 'blue') {
-                $(this).addClass('blue');
-                // $h2Span.html('Yellow');
+                $(`#${spanID}`).addClass('blue');
                 $h2Span.removeClass('blue');
                 $h2Span.addClass('yellow');
                 $h2Span.prop('id', 'yellow');
                 tictactoeApp.spanIDObj[spanID] = 'blue';
             }
             else {
-                $(this).addClass('yellow');
-                // $h2Span.html('Blue');
+                $(`#${spanID}`).addClass('yellow');
                 $h2Span.removeClass('yellow');
                 $h2Span.addClass('blue');
                 $h2Span.prop('id', 'blue');
@@ -33,10 +56,8 @@ tictactoeApp.play = () => {
             }
             tictactoeApp.count++;
             tictactoeApp.match();
-            // console.log(tictactoeApp.count);
-            // console.log(tictactoeApp.spanIDObj);
         }
-    })
+    }
 }
 
 tictactoeApp.random = () => {
@@ -51,69 +72,156 @@ tictactoeApp.random = () => {
     }
     console.log(arrRandom);
     // console.log($('input[type=checkbox]').prop('checked'));
-    $('.random').on('click', () => {
+
+    $('.random').on('change', () => {
         if ($('input[type=checkbox]').prop('checked')) {
             $('input[type=checkbox]').attr('disabled', true);
             $gridSpan.off('click');
-            $gridSpan.on('click', () => {
-                if (clickCount === 0) {
-                    $(`#${arrRandom[0]}`).addClass('blue');
-                    tictactoeApp.spanIDObj[`${arrRandom[0]}`] = 'blue';
-                    if (!tictactoeApp.match()) {
-                        $(`#${arrRandom[1]}`).addClass('yellow');
-                        tictactoeApp.spanIDObj[`${arrRandom[1]}`] = 'yellow';
-                    }
-                    clickCount++;
-                    tictactoeApp.match();
-                }
-                else if (clickCount === 1) {
-                    $(`#${arrRandom[2]}`).addClass('blue');
-                    tictactoeApp.spanIDObj[`${arrRandom[2]}`] = 'blue';
-                    if (!tictactoeApp.match()) {
-                        $(`#${arrRandom[3]}`).addClass('yellow');
-                        tictactoeApp.spanIDObj[`${arrRandom[3]}`] = 'yellow';
-                    }
-                    clickCount++;
-                    tictactoeApp.match();
-                }
-                else if (clickCount === 2) {
-                    $(`#${arrRandom[4]}`).addClass('blue');
-                    tictactoeApp.spanIDObj[`${arrRandom[4]}`] = 'blue';
-                    if (!tictactoeApp.match()) {
-                        $(`#${arrRandom[5]}`).addClass('yellow');
-                        tictactoeApp.spanIDObj[`${arrRandom[5]}`] = 'yellow';
-                    }
-                    clickCount++;
-                    tictactoeApp.match();
-                }
-                else if (clickCount === 3) {
-                    $(`#${arrRandom[6]}`).addClass('blue');
-                    tictactoeApp.spanIDObj[`${arrRandom[6]}`] = 'blue';
-                    if (!tictactoeApp.match()) {
-                        tictactoeApp.spanIDObj[`${arrRandom[7]}`] = 'yellow';
-                        $(`#${arrRandom[7]}`).addClass('yellow');
-                    }
-                    clickCount++;
-                    tictactoeApp.match();
-                }
-                else if (clickCount === 4) {
-                    $(`#${arrRandom[8]}`).addClass('blue');
-                    // $(`#${arrRandom[1]}`).addClass('yellow');
-                    tictactoeApp.spanIDObj[`${arrRandom[8]}`] = 'blue';
-                    // tictactoeApp.spanIDObj[1] = 'yellow';
-                    // clickCount++;
-                    tictactoeApp.count = 9;
-                    tictactoeApp.match();
-                    // $gridSpan.off('click');
-                }
-                console.log(tictactoeApp.spanIDObj);
-            })
+            // console.log(true);
         }
-        else {
-            // tictactoeApp.play();
-            // location.reload();
-        }
+        $gridSpan.keypress(function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                // spanID = $(this).attr('id');
+                if (!tictactoeApp.match()) {
+                    game();
+
+                    tictactoeApp.match();
+                }
+            }
+        })
+        $gridSpan.on('click', () => {
+            // game2();
+            if (!tictactoeApp.match()) {
+                game();
+
+                tictactoeApp.match();
+            }
+            else {
+                $gridSpan.off('click');
+            }
+        })
     })
+
+
+    const game = () => {
+        if (clickCount === 0) {
+            $(`#${arrRandom[0]}`).addClass('blue');
+            tictactoeApp.spanIDObj[`${arrRandom[0]}`] = 'blue';
+            if (!tictactoeApp.match()) {
+                $(`#${arrRandom[1]}`).addClass('yellow');
+                tictactoeApp.spanIDObj[`${arrRandom[1]}`] = 'yellow';
+            }
+            clickCount++;
+            // tictactoeApp.match();
+        }
+        else if (clickCount === 1) {
+            $(`#${arrRandom[2]}`).addClass('blue');
+            tictactoeApp.spanIDObj[`${arrRandom[2]}`] = 'blue';
+            if (!tictactoeApp.match()) {
+                $(`#${arrRandom[3]}`).addClass('yellow');
+                tictactoeApp.spanIDObj[`${arrRandom[3]}`] = 'yellow';
+            }
+            clickCount++;
+            // tictactoeApp.match();
+        }
+        else if (clickCount === 2) {
+            $(`#${arrRandom[4]}`).addClass('blue');
+            tictactoeApp.spanIDObj[`${arrRandom[4]}`] = 'blue';
+            if (!tictactoeApp.match()) {
+                $(`#${arrRandom[5]}`).addClass('yellow');
+                tictactoeApp.spanIDObj[`${arrRandom[5]}`] = 'yellow';
+            }
+            clickCount++;
+            // tictactoeApp.match();
+        }
+        else if (clickCount === 3) {
+            $(`#${arrRandom[6]}`).addClass('blue');
+            tictactoeApp.spanIDObj[`${arrRandom[6]}`] = 'blue';
+            if (!tictactoeApp.match()) {
+                tictactoeApp.spanIDObj[`${arrRandom[7]}`] = 'yellow';
+                $(`#${arrRandom[7]}`).addClass('yellow');
+            }
+            clickCount++;
+            // tictactoeApp.match();
+        }
+        else if (clickCount === 4) {
+            $(`#${arrRandom[8]}`).addClass('blue');
+            // $(`#${arrRandom[1]}`).addClass('yellow');
+            tictactoeApp.spanIDObj[`${arrRandom[8]}`] = 'blue';
+            // tictactoeApp.spanIDObj[1] = 'yellow';
+            // clickCount++;
+            tictactoeApp.count = 9;
+            // tictactoeApp.match();
+            // $gridSpan.off('click');
+        }
+        console.log(tictactoeApp.spanIDObj);
+    }
+
+    //turn off event listeners
+    // $('.random').on('click', () => {
+    //     if ($('input[type=checkbox]').prop('checked')) {
+    //         $('input[type=checkbox]').attr('disabled', true);
+    //         $gridSpan.off('click');
+    //         $gridSpan.on('click', () => {
+    //             if (clickCount === 0) {
+    //                 $(`#${arrRandom[0]}`).addClass('blue');
+    //                 tictactoeApp.spanIDObj[`${arrRandom[0]}`] = 'blue';
+    //                 if (!tictactoeApp.match()) {
+    //                     $(`#${arrRandom[1]}`).addClass('yellow');
+    //                     tictactoeApp.spanIDObj[`${arrRandom[1]}`] = 'yellow';
+    //                 }
+    //                 clickCount++;
+    //                 tictactoeApp.match();
+    //             }
+    //             else if (clickCount === 1) {
+    //                 $(`#${arrRandom[2]}`).addClass('blue');
+    //                 tictactoeApp.spanIDObj[`${arrRandom[2]}`] = 'blue';
+    //                 if (!tictactoeApp.match()) {
+    //                     $(`#${arrRandom[3]}`).addClass('yellow');
+    //                     tictactoeApp.spanIDObj[`${arrRandom[3]}`] = 'yellow';
+    //                 }
+    //                 clickCount++;
+    //                 tictactoeApp.match();
+    //             }
+    //             else if (clickCount === 2) {
+    //                 $(`#${arrRandom[4]}`).addClass('blue');
+    //                 tictactoeApp.spanIDObj[`${arrRandom[4]}`] = 'blue';
+    //                 if (!tictactoeApp.match()) {
+    //                     $(`#${arrRandom[5]}`).addClass('yellow');
+    //                     tictactoeApp.spanIDObj[`${arrRandom[5]}`] = 'yellow';
+    //                 }
+    //                 clickCount++;
+    //                 tictactoeApp.match();
+    //             }
+    //             else if (clickCount === 3) {
+    //                 $(`#${arrRandom[6]}`).addClass('blue');
+    //                 tictactoeApp.spanIDObj[`${arrRandom[6]}`] = 'blue';
+    //                 if (!tictactoeApp.match()) {
+    //                     tictactoeApp.spanIDObj[`${arrRandom[7]}`] = 'yellow';
+    //                     $(`#${arrRandom[7]}`).addClass('yellow');
+    //                 }
+    //                 clickCount++;
+    //                 tictactoeApp.match();
+    //             }
+    //             else if (clickCount === 4) {
+    //                 $(`#${arrRandom[8]}`).addClass('blue');
+    //                 // $(`#${arrRandom[1]}`).addClass('yellow');
+    //                 tictactoeApp.spanIDObj[`${arrRandom[8]}`] = 'blue';
+    //                 // tictactoeApp.spanIDObj[1] = 'yellow';
+    //                 // clickCount++;
+    //                 tictactoeApp.count = 9;
+    //                 tictactoeApp.match();
+    //                 // $gridSpan.off('click');
+    //             }
+    //             console.log(tictactoeApp.spanIDObj);
+    //         })
+    //     }
+    //     else {
+    //         // tictactoeApp.play();
+    //         // location.reload();
+    //     }
+    // })
 }
 
 tictactoeApp.match = () => {
@@ -127,8 +235,7 @@ tictactoeApp.match = () => {
         (tictactoeApp.spanIDObj[1] === 'blue' && tictactoeApp.spanIDObj[5] === 'blue' && tictactoeApp.spanIDObj[9] === 'blue') || 
         (tictactoeApp.spanIDObj[3] === 'blue' && tictactoeApp.spanIDObj[5] === 'blue' && tictactoeApp.spanIDObj[7] === 'blue')) {
         $h2.html(`Player Won: <span class="blue">blue</span>`);
-        $gridSpan.off('click');
-        //checl
+        // $gridSpan.off('click');
         return true;
     } else
     if ((tictactoeApp.spanIDObj[1] === 'yellow' && tictactoeApp.spanIDObj[2] === 'yellow' && tictactoeApp.spanIDObj[3] === 'yellow') ||
@@ -140,13 +247,13 @@ tictactoeApp.match = () => {
         (tictactoeApp.spanIDObj[1] === 'yellow' && tictactoeApp.spanIDObj[5] === 'yellow' && tictactoeApp.spanIDObj[9] === 'yellow') ||
         (tictactoeApp.spanIDObj[3] === 'yellow' && tictactoeApp.spanIDObj[5] === 'yellow' && tictactoeApp.spanIDObj[7] === 'yellow')) {
         $h2.html(`Player Won: <span class="yellow">blue</span>`);
-        $gridSpan.off('click');
+        // $gridSpan.off('click');
         return true;
     }
     else if (tictactoeApp.count === 9) {
         $h2.html('Player Won: TIE');
-        $gridSpan.off('click');
-        // return true;
+        // $gridSpan.off('click');
+        return true;
     }
 }
 
